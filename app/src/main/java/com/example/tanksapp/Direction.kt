06ba6e03305568d2f4ddbbabdda7cc622e791392
Шигалева Александра Валерьевна <2947-22@std.pkgh.edu>
2023-11-13 11:56:08 +0300

    not screen border

---
+++ app/src/main/java/com/example/tanksapp/Direction.kt
@@ -0,0 +1,8 @@
+package com.example.tanksapp
+
+enum class Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+}
---
+++ app/src/main/java/com/example/tanksapp/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.tanksapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import  android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_DPAD_DOWN
+import android.view.KeyEvent.KEYCODE_DPAD_LEFT
+import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
+import android.view.KeyEvent.KEYCODE_DPAD_UP
+import android.widget.FrameLayout
+import com.example.tanksapp.Direction.UP
+import com.example.tanksapp.Direction.DOWN
+import com.example.tanksapp.Direction.LEFT
+import com.example.tanksapp.Direction.RIGHT
+import com.example.tanksapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onKeyDown(keyCode:Int, event: KeyEvent?):Boolean{
+        when(keyCode) {
+            KEYCODE_DPAD_UP -> move(UP)
+            KEYCODE_DPAD_DOWN -> move(DOWN)
+            KEYCODE_DPAD_LEFT -> move(LEFT)
+            KEYCODE_DPAD_RIGHT -> move(RIGHT)
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun move(direction: Direction) {
+        when(direction) {
+            UP -> {
+                binding.myTank.rotation = 0f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
+            }
+            DOWN -> {
+                binding.myTank.rotation = 100f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+            }
+            LEFT -> {
+                binding.myTank.rotation = 270f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= 50
+            }
+            RIGHT -> {
+                binding.myTank.rotation = 90f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += 50
+            }
+        }
+        binding.container.removeView(binding.myTank)
+        binding.container.addView(binding.myTank)
+    }
+}
